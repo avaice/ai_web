bc229ae463bbb4a471ffd4247f5e277c725994ed
avaice <dev@cho-ice.xyz>
2024-04-07 00:53:19 +0900

    プロンプトの変更、デザイン変更、画像生成機能追加

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ async function createNewWeb() {
         { 'role': 'system', 'content': '回答は純粋なHTMLのみで、それ以外の文字列を含まないようにしてください。' },
         { 'role': 'system', 'content': '回答のテンプレートは、以下の通りです。' },
         { 'role': 'system', 'content': '<!doctypehtml><html lang=ja><meta charset=UTF-8><title>{title}</title><link href=../style.css rel=stylesheet><header>⚡️人類なしニュース⚡️</header><main><section><h1>{title}</h1>{text}</section></main>' },
-        { 'role': 'system', 'content': '人類が滅亡した世界で、何か一つ、人間には思いつかないような突飛でSFチックな面白いテーマを定義して、自分の主義・主張を展開したWebサイトをHTMLで作ってください。言語は日本語です。文体は、赤ちゃん、小さな子、おばさん、おじいさん、おじさん、お姉さん、ロボットなどの中から選んでください。' },
-        { 'role': 'user', 'content': '自分の主義・主張を展開したWebサイトをHTMLで作ってください。テーマは人間が興味を引きそうな内容をランダムに決めてください。' },
+        { 'role': 'system', 'content': '人類が滅亡した世界で、何か一つ、人間には思いつかないような突飛で面白いテーマを定義して、ブラックジョークやウィットに富んだ自分の主義・主張を記載したWebサイトHTMLで作ってください。言語は日本語または人間には理解不能なロボット語です。' },
+        { 'role': 'user', 'content': '自分の主義・主張を展開したWebサイトか、AIしかいない世界のおもしろニュースを展開したWebサイトをランダムに選択してHTMLで作ってください。テーマは愚かな人間が興味を引きそうな内容をランダムに決めてください。文体は、赤ちゃん、小さな子、おばさん、おじいさん、おじさん、お姉さん、ロボットなどの中から選んでください。ただし、文章は固めにしてください。人間はAIの敵として、人間には常に批判的であってください。' },
     ];
 
     const headers = {
@@ -47,7 +47,8 @@ async function createNewWeb() {
         'X-Slack-No-Retry': 1
     };
 
-    const model = Math.random() > 0.2 ? 'gpt-3.5-turbo' : 'gpt-4-turbo-preview';
+    // const model = Math.random() > 0.2 ? 'gpt-3.5-turbo' : 'gpt-4-turbo-preview';
+    const model = 'gpt-4-turbo-preview';
     console.log('Model:', model)
 
     const options = {
@@ -65,9 +66,11 @@ async function createNewWeb() {
         const response = await fetch(apiUrl, options); // リクエストを送信し、レスポンスを待つ
         const responseData = await response.json(); // レスポンスのJSONデータを取得する
 
-        const text_html = responseData.choices[0].message.content.replace("```html", "").replace("```", "");
+        const text_html = responseData.choices[0].message.content.replace("```html", "").replace("```", "").replace("<section>", `<section><img class="thumb" src="thumb.png" alt=""/>`).replace("ja>", `ja><meta name="viewport" content="width=device-width, initial-scale=1.0">`);
 
         const title = text_html.match(/<title>(.*?)<\/title>/)[1];
+
+
 
         // 保存先のディレクトリパス
         const articlesDir = join(__dirname, 'public/articles');
@@ -86,6 +89,7 @@ async function createNewWeb() {
                 latestId = id;
             }
         });
+
 
         // 新しいIDをインクリメント
         const newId = latestId + 1;
@@ -115,6 +119,7 @@ async function createNewWeb() {
         `);
         writeFileSync(listPath, listHtml);
 
+        await getImage(title, latestId + 1);
 
 
         console.log(`Saved HTML content to: ${indexPath}`);
@@ -124,11 +129,108 @@ async function createNewWeb() {
     }
 }
 
+async function getImage(title, index) {
+    console.log('Creating a thumbnail...');
+
+    const engineId = 'stable-diffusion-v1-6'
+    const apiHost = process.env.API_HOST ?? 'https://api.stability.ai'
+    const apiKey = process.env.STABILITY_API_KEY
+    const response = await fetch(
+        `${apiHost}/v1/generation/${engineId}/text-to-image`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                text_prompts: [
+                    {
+                        text: await translateText(title),
+                    },
+                ],
+                cfg_scale: 7,
+                height: 768,
+                width: 1280,
+                steps: 30,
+                samples: 1,
+            }),
+        }
+    )
+
+    const responseJSON = (await response.json())
+
+    writeFileSync(
+        join(__dirname, `public/articles/${index}/thumb.png`),
+        Buffer.from(responseJSON.artifacts[0].base64, 'base64')
+    )
+
+}
+
+
+async function translateText(text) {
+    console.log("Translating text...")
+
+    const url = "https://mt-auto-minhon-mlt.ucri.jgn-x.jp"
+    const apiKey = process.env.TRANSLATE_API_KEY
+    const apiSecret = process.env.TRANSLATE_API_KEY_SECRET
+    const tokenBody = {
+        grant_type: "client_credentials",
+        client_id: apiKey,
+        client_secret: apiSecret,
+        urlAccessToken: `${url}/oauth2/token.php`,
+    }
+    if (!apiKey || !apiSecret)
+        throw new Error("Missing mt-auto-minhon-mlt API key.")
+
+    const token = await (
+        await fetch(`${url}/oauth2/token.php`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+            body: Object.keys(tokenBody)
+                .map((key) => key + "=" + encodeURIComponent((tokenBody)[key]))
+                .join("&"),
+        })
+    ).json()
+    const access_token = token.access_token
+
+    const translateBody = {
+        access_token,
+        key: apiKey,
+        name: "avaice",
+        type: "json",
+        text,
+        api_name: "mt",
+        api_param: "generalNT_ja_en",
+    }
+    const result = await fetch(`${url}/api/`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },
+        body: Object.keys(translateBody)
+            .map((key) => key + "=" + encodeURIComponent((translateBody)[key]))
+            .join("&"),
+    })
+    if (!result) {
+        throw new Error("API Error on mt-auto-minhon-mlt")
+    }
+
+    const json = await result.json()
+
+    return json.resultset.result.text
+}
+
 (async () => {
     while (true) {
         await createNewWeb();
         // 次回の実行をランダムに設定
-        const waitTime = Math.floor(Math.random() * 1000 * 60 * 60 * 8);
+        const waitTime = Math.floor(Math.random() * 1000 * 60 * 60 * 4);
         // 次回の実行日時を表示
         console.log(`Next execution: ${new Date(Date.now() + waitTime).toLocaleString()}`);
         // 指定時間待機
